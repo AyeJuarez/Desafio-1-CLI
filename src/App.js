c0ed4d0a792ef78c8704e82aa33977391c04e622
Ayelen Juarez <ayelenjuarez@hotmail.com>
2022-08-22 19:33:49 -0300

    corrigiendo rutas

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import React from 'react';
 import './style.css';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import NavBarExample from "./components/NavBar";
-import contact from './pages/contact';
-import home from './pages/home';
-import products from './pages/products';
+import Contact from './pages/contact';
+import Home from './pages/home';
+import Products from './pages/products';
 
 
 function App() {
     return (
       <div className='App'>
-        <h1>Bienvenido a Jabali Motors!</h1>
         <Router>
           <Routes>
             <Route path='/' element={ <NavBarExample />}>
-              <Route path index element={ <home/>}/>
-              <Route path='contact' element={ <contact/>}/>
-              <Route path='products' element={ <products/>}/>
+              <Route path index element={ <Home/>}/>
+              <Route path='contact' element={ <Contact/>}/>
+              <Route path='products' element={ <Products/>}/>
 
               <Route path='*' element={ <Navigate replace to="/" />}/>
             </Route>
