41402e5189253c722006854426390f6c1f04892c
Ayelen Juarez <ayelenjuarez@hotmail.com>
2022-08-22 18:57:48 -0300

    Agregando NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import NavBarExample from "./components/NavBar";
+import contact from './pages/contact';
+import home from './pages/home';
+import products from './pages/products';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Jabali Motors</p>
-        <p> Ayelen L. Juarez</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div className='App'>
+        <h1>Bienvenido a Jabali Motors!</h1>
+        <Router>
+          <Routes>
+            <Route path='/' element={ <NavBarExample />}>
+              <Route path index element={ <home/>}/>
+              <Route path='contact' element={ <contact/>}/>
+              <Route path='products' element={ <products/>}/>
+
+              <Route path='*' element={ <Navigate replace to="/" />}/>
+            </Route>
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+
 
 export default App;
