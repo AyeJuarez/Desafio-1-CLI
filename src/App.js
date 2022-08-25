7cd552c49961c1c8e8bfb07bf8b837046a1ea1f4
Ayelen Juarez <ayelenjuarez@hotmail.com>
2022-08-25 20:55:07 -0300

    entrega Desafio 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './style.css';
+
+// Importaciones del Desafio 2
+/*
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import NavBarExample from "./components/NavBar";
 import Contact from './pages/contact';
 import Home from './pages/home';
 import Products from './pages/products';
+*/
 
+//Importacion del Desafio 3
+import CardComponent from './components/CardComponent/CardComponent';
+import CartWidget from './components/CartWidget/CartWidget';
+import campera from './components/CardComponent/campera.jpg';
+import campera1 from './components/CardComponent/campera1.jpg';
+import campera2 from './components/CardComponent/campera2.jpg';
 
-function App() {
+/*function App() {
     return (
       <div className='App'>
         <Router>
@@ -23,6 +33,38 @@ function App() {
         </Router>
       </div>
     )
+  }*/
+
+// Desafio 3 Componentes
+
+  class App extends React.Component{
+    render (){
+      return (
+        <div>
+          <CartWidget greeting='Bienvenid@!' />
+          <div className='UserSection'>
+              <CardComponent 
+              name='CAMPERA HELD MUJER'
+              description='Cuello suave, cremalleras de ventilación y estiramiento en la parte interior del brazo garantizan una gran comodidad de uso. Está disponible 3 colores diferentes.'
+              img={campera}
+              price='Precio: $240.000'
+              />
+              <CardComponent 
+              name='CAMPERA ESTILO TOURING MUJER'
+              description='Campera de mujer para motos de touring con corte a medida. Particularmente cómoda de llevar permite ajustar el ancho del brazo, la cintura y la cadera.'
+              img={campera1}
+              price='Precio: $164.000'
+              />
+              <CardComponent 
+              name='CAMPERA TUCANO URBANO'
+              description='Cazadora cuatro estaciones de corte medio, transpirable, cortaviento e impermeable, corte femenino'
+              img={campera2}
+              price='Precio: $70.000'
+              />
+          </div>
+        </div>
+      )
+    }
   }
 
 
---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../CartWidget/CartWidget.css';
+import logo from './logo.png';
+
+const CartWidget = (props) => {
+    return (
+        <div className='CartWidget'>
+            <img src={logo} alt='logo' />
+            <h1>{ props.greeting }</h1>
+        </div>
+        
+    )
+}
+
+export default CartWidget
